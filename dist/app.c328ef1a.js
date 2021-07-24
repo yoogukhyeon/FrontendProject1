f9f0a22e622cac8d0879db3993a64b825c7a1a34
유국현 <rnrgus5897@gmail.com>
2021-07-25 00:20:11 +0900

    2021 07 / 25 frontend 12줄로 api 만들기

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -118,7 +118,28 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"app.js":[function(require,module,exports) {
+//html에서 container 태그를 가져옴
+var container = document.getElementById('root'); //서버 통신을 위해서 XMLHttpRequest() 변수 ajax 담는다 
 
+var ajax = new XMLHttpRequest(); //ajax 통신 open , send , response
+//ajx.open('method 방식' , 'api url' , 비동기통신 안한다는뜻입니다)
+
+ajax.open('GET', '	https://api.hnpwa.com/v0/news/1.json', false);
+ajax.send(); //JOSON 형태로 값을 받는다
+
+var newsFeed = JSON.parse(ajax.response); //createElement 생성 
+
+var ul = document.createElement('ul'); //for문으로 먼저 10의 title값을 가져오기
+
+for (var i = 0; i < 10; i++) {
+  var li = document.createElement('li');
+  li.innerHTML = newsFeed[i].title; // ul태그안에 자식 li 넣는다 appenChild 자식을 넣기위한 메소드
+
+  ul.appendChild(li);
+} //container 넣음으로 결과 값이 잘나온다!
+
+
+container.appendChild(ul);
 },{}],"../../Users/유국현/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -147,7 +168,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65452" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55592" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
