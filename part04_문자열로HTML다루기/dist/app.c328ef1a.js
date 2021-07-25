2b5f1ca29a0cfb4512c4199e6215f0c31b0e29db
유국현 <rnrgus5897@gmail.com>
2021-07-25 11:27:41 +0900

    문자열로 html 다루기

--- part04_문자열로HTML다루기/dist/app.c328ef1a.js
+++ part04_문자열로HTML다루기/dist/app.c328ef1a.js
@@ -194,7 +194,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52654" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61608" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
