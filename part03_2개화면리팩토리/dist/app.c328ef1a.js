3cd3f257c5ac6bdf3ac4c4bd988ae2e318b42d6b
유국현 <rnrgus5897@gmail.com>
2021-07-29 14:32:41 +0900

    part3 2개 화면 리팩토리

--- part03_2개화면리팩토리/dist/app.c328ef1a.js
+++ part03_2개화면리팩토리/dist/app.c328ef1a.js
@@ -118,56 +118,43 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"app.js":[function(require,module,exports) {
-//중복된 코드를 리팩토리 
-//html에서 container 태그를 가져옴
-var container = document.getElementById('root'); //서버 통신을 위해서 XMLHttpRequest() 변수 ajax 담는다 
-
+//2개화면 리팩토리 
+var container = document.getElementById('root');
 var ajax = new XMLHttpRequest();
+var content = document.createElement('div');
 var NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
-var CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json'; //중복되는 AJAX 함수를 만들기 바뀌는 부분은 URL 밖에 없기 떄문에 인자로 URL 넣고 호출할떄마다 URL 바꿔준다
+var CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json'; //반복코드 getDate함수안에 ajax 코드를 넣고 url바뀔수 있어 인자 값에 url을 줍니다
 
-function getDate(URL) {
-  //ajax 통신 open , send , response
-  //ajx.open('method 방식' , 'api url' , 비동기통신 안한다는뜻입니다)
-  ajax.open('GET', URL, false);
-  ajax.send(); //JOSON 형태로 return 값을 받는다
+function getDate(url) {
+  ajax.open('GET', url, false);
+  ajax.send(); //ajax.response 반환값 return
 
   return JSON.parse(ajax.response);
 }
 
-; //div 생성
+; //ajax getDate 함수를 통해서 불러와서 코드를 줄인다
 
 var newsFeed = getDate(NEWS_URL);
-var Content = document.createElement('div'); //createElement 생성 
-
-var ul = document.createElement('ul'); //hash event 생성
-//hashchange 이벤트는 같은 페이지 안에서 해시(#)만 바뀌었을 때, 즉 페이지에서 id가 있는 요소로 이동하게 하기위해서 hash를 쓴다 
-
+var ul = document.createElement('ul');
 window.addEventListener('hashchange', function () {
-  //newsContent API url 보면 @id 부분에 @지우기 위해 substr 썻다
   var id = location.hash.substr(1);
-  console.log('hash 변경됨'); //hash 값을 구하기위해서 replace 메소드를 속성을 써거 첫번쨰 인자를 두번쨰 인자로 바꾼다.
-
   var newsContent = getDate(CONTENT_URL.replace('@id', id));
   var title = document.createElement('h1');
   title.innerHTML = newsContent.title;
-  Content.appendChild(title);
-}); //for문으로 먼저 10의 title값을 가져오기
+  content.appendChild(title);
+});
 
 for (var i = 0; i < 10; i++) {
   var li = document.createElement('li');
   var a = document.createElement('a');
   a.href = "#".concat(newsFeed[i].id);
-  a.innerHTML = "".concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")"); // a Element 생성 후 값들을 넣어주고 li appendChild 자식으로 넣어준다
-  // ul태그안에 자식 li 넣는다 appenChild 자식을 넣기위한 메소드
-
+  a.innerHTML = "".concat(newsFeed[i].title, " (").concat(newsFeed[i].comments_count, ")");
   li.appendChild(a);
   ul.appendChild(li);
-} //container 넣음으로 결과 값이 잘나온다!
-
+}
 
 container.appendChild(ul);
-container.appendChild(Content);
+container.appendChild(content);
 },{}],"../../../Users/유국현/AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -196,7 +183,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57850" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53320" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
